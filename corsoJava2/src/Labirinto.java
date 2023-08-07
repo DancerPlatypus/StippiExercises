1f8a74cfb7f59bf5cdd5aa4770ee21c9e99b718c
Mattia Barini <>
2023-08-07 13:03:43 +0200

    Added 2nd ctor in Labirinto

--- corsoJava2/src/Labirinto.java
+++ corsoJava2/src/Labirinto.java
@@ -4,15 +4,24 @@ public class Labirinto {
     int upperbound = 2;
     char matrix[][] = null;
 
+    private int nRows=10;
+    private int nColumns=10;
+
     public Labirinto(){
+        InitMatrix();
+    }
+
+    public Labirinto(int nRows,int nColumns){
+        this.nRows=nRows;
+        this.nColumns = nColumns;
         InitMatrix();
     }
 
     private void InitMatrix(){
         Random rand = new Random();
-        matrix = new char[10][10];
-        for (int i=0;i<10;i++){
-            for (int j = 0; j < 10; j++) {
+        matrix = new char[nRows][nColumns];
+        for (int i=0;i<nRows;i++){
+            for (int j = 0; j < nColumns; j++) {
 
                 if(rand.nextInt(upperbound)==1){
                     matrix[i][j]='X';
