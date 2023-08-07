3fc8998509d15909658826f4f2fb289c1c9579fd
DESKTOP-R7A3MHP\Matteo <matteo.stopazzola.ms@gmail.com>
2023-08-07 17:26:29 +0200

    no message

--- corsoJava2/src/Labirinto.java
+++ corsoJava2/src/Labirinto.java
@@ -43,21 +43,43 @@ public class Labirinto {
             System.out.println();
         }
     }
-    
     public boolean CanStart(int a,int b){
         if(a<0 || b<0){
             return false;
         }
-
         if(a!=0 && b!=0 ){
             return false;
         }
-
         if(matrix[a][b]=='X'){
             return false;
         }
-
         return true;
+    }
 
+    public boolean Finish(int a, int b){
+        if(matrix[a][b] == 0){
+            return true;
+        }
+        return false;
+    }
+
+    public void findPath(int a, int b){
+        CanStart(a, b);
+        for(int i = 0; i<=nRows; i++){
+            for (int j = 0; j<=nColumns; j++){
+                if(matrix[a][b] == 'X'){
+                    a = a + 1;
+                    b = b + 1;
+                }
+                else {
+                    a = a - 1;
+                    b = b - 1;
+                }
+
+                if(matrix[a][b] == '0'){
+                    matrix[a][b] = '1';
+                }
+            }
+        }
     }
 }
