cd8a7006dbdc9766876ad2295edce234daccc68d
Mattia Barini <>
2023-08-07 12:53:23 +0200

    Labirinto new

--- corsoJava2/src/Labirinto.java
+++ corsoJava2/src/Labirinto.java
@@ -1,2 +1,38 @@
+import  java.util.Random;
+
 public class Labirinto {
+    int upperbound = 2;
+    char matrix[][] = null;
+
+    public Labirinto(){
+        InitMatrix();
+    }
+
+    private void InitMatrix(){
+        Random rand = new Random();
+        matrix = new char[10][10];
+        for (int i=0;i<10;i++){
+            for (int j = 0; j < 10; j++) {
+
+                if(rand.nextInt(upperbound)==1){
+                    matrix[i][j]='X';
+                }
+                else{
+                    matrix[i][j]='0';
+                }
+            }
+        }
+    }
+
+    public void PrintMatrix(){
+        for (int i=0;i<10;i++){
+            for (int j = 0; j < 10; j++) {
+
+                System.out.print(matrix[i][j]);
+                System.out.print(' ');
+            }
+            System.out.println();
+        }
+    }
+
 }
