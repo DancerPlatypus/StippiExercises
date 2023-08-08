a7c13e198442ee9ccdc4fc3588421216ad9b55b8
DESKTOP-R7A3MHP\Matteo <matteo.stopazzola.ms@gmail.com>
2023-08-08 17:56:10 +0200

    metodo valutaRiga

--- corsoJava2/src/GiocoDelTris.java
+++ corsoJava2/src/GiocoDelTris.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class GiocoDelTris {
     private char[][] griglia;
 
@@ -5,7 +7,25 @@ public class GiocoDelTris {
         this.griglia = new char[3][3];
     }
 
-    public void faiUnaMossa(){
+    public boolean valutaRiga(){
+        boolean controllo = false;
+        for(int i = 0; i<this.griglia.length; i++) {
+            if(this.griglia[i][0] == 'X' || this.griglia[i][0] == 'O') {
+                controllo = true;
+                break;
+            }
+        }
+        return controllo;
+    }
+
+    public void condizioneFinale(){
+
+
+    }
+    public void faiUnaMossa(char nuovaMossa){
+        Scanner tastiera = new Scanner(System.in);
+        System.out.println("Seleziona la casella");
+
 
     }
 }
