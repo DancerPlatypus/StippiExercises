89ceccb6719377b9d80c46a19e9b85032f0f0462
Mattia Barini <>
2023-08-08 19:00:25 +0200

    Update classe

--- corsoJava2/src/GiocoDelTris.java
+++ corsoJava2/src/GiocoDelTris.java
@@ -4,20 +4,69 @@ public class GiocoDelTris {
     private char[][] griglia;
 
     public GiocoDelTris(){
-        this.griglia = new char[3][3];
+        initGriglia();
     }
 
-    //Sei sicuro di valutare la riga??????
+    public void initGriglia(){
+        this.griglia = new char[][]{{'X', 'O', 'X'}, {'O', 'O', 'O'},{'O', 'O', 'X'}};
+    }
+
+    public void stampaOutput(){
+        for(int i = 0; i<this.griglia.length; i++) {
+            for (int j = 0; j < this.griglia.length; j++) {
+                System.out.print(this.griglia[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
     public boolean valutaRiga(){
         boolean controllo = false;
+
         for(int i = 0; i<this.griglia.length; i++) {
-            if(this.griglia[i][0] == 'X' || this.griglia[i][0] == 'O') {
-                controllo = true;
-                break;
+            for (int j = 1; j<this.griglia.length; j++){
+                if(this.griglia[i][j] == this.griglia[i][j-1]) {
+                    controllo = true;
+                    break;
+                }
+                else {
+                    i++;
+                }
             }
         }
         return controllo;
     }
+    public boolean valutaColonna(){
+        boolean controllo = false;
+
+        for(int j = 0; j<this.griglia.length; j++) {
+            for (int i = 1; i<this.griglia.length; i++){
+                if(this.griglia[i][j] == this.griglia[i-1][j]) {
+                    controllo = true;
+                    break;
+                }
+                else {
+                    j++;
+                }
+            }
+        }
+        return controllo;
+    }
+
+    public boolean valutaDiagonali(){
+        boolean controllo = false;
+        for(int i = 0; i<this.griglia.length; i++){
+            if(this.griglia[i][i] == this.griglia[i-1][i-1]){
+                controllo = true;
+            }
+            else if (this.griglia[i][i] == this.griglia[i+1][i-1]){
+                controllo = true;
+            }
+        }
+        return controllo;
+    }
+
 
     public void condizioneFinale(){
 
